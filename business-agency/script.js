2c21901a4f8387379dbffa362c87a667244e8737
kno-be <beknoblauch18@gmail.com>
2021-08-09 17:35:36 -0300

    upd: working navbar

---
+++ business-agency/script.js
@@ -0,0 +1,10 @@
+// NAVDAR
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+    menu.classList.toggle('change');
+})
+// END OF NAVBAR
+
